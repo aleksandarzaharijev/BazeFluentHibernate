62cb5cdbbaf96facdd883e4052c3bf0619b03496
Aleksandar Zaharijev <azaharijev@elfak.rs>
2022-05-07 23:11:08 +0200

    Prvi

---
+++ ProjekatBaze2/ProjekatBaze2/Entiteti/Banka.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjekatBaze2.Entiteti
+{
+    public class Banka
+    {
+        public virtual int Id { get; set; }
+        public virtual string AdresaCentrale { get; set; }
+        public virtual string WebAdresa { get; set; }
+        public virtual string Ime { get; set; }
+        public virtual string Email { get; set; }   
+    }
+}
---
+++ ProjekatBaze2/ProjekatBaze2/Entiteti/Bankomat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjekatBaze2.Entiteti
+{
+    public class Bankomat
+    {
+        public virtual int Id { get; set; } 
+        public virtual string Lokacija { get; set; }  
+        public virtual string NazivProizvodjaca { get; set; }
+
+        public virtual char Status { get; set; }   
+        public virtual DateTime DatumPoslednjegServisa { get; set; }    
+        public virtual int BrojFilijala { get; set; }
+    }
+}
